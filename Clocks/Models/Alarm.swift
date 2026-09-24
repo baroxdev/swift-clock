c0e305e02abdcbf32e5d8efd34bd1bd2939e5644
baroxdev <barox.dev@gmail.com>
2026-09-24 23:12:39 +0700

    init project

---
+++ Clocks/Models/Alarm.swift
@@ -0,0 +1,8 @@
+//
+//  Alarm.swift
+//  Clocks
+//
+//  Created by admin on 24/9/26.
+//
+
+import Foundation
---
+++ Clocks/Views/CreateAlarmView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAlarmView.swift
+//  Clocks
+//
+//  Created by admin on 23/9/26.
+//
+
+import SwiftUI
+
+struct CreateAlarmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAlarmView()
+}
